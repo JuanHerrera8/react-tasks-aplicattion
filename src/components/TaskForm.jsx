707ca75c99cf79ba3613fb9369ba547b0080dea1
JuanPC <herrera.francojd@gmail.com>
2022-09-29 20:11:43 -0500

    Añadiendo Estilos

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -18,24 +18,29 @@ function TaskForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder='Escribe tu tarea'
-        onChange={(e) => {
-          setTitle(e.target.value)
-        }}
-        value={title}
-        autoFocus
-      />
-      <textarea
-        placeholder='Escribe una descripción'
-        onChange={(e) => {
-          setDescription(e.target.value)
-        }}
-        value={description}
-      ></textarea>
-      <button>Guardar</button>
-    </form>
+    <div className='max-w-md mx-auto'>
+      <form className='bg-slate-800 p-10 mb-4' onSubmit={handleSubmit}>
+        <h1 className='text-2xl font-bold text-white mb-3'>Crea tu tarea</h1>
+        <input
+          className='bg-slate-300 p-3 w-full mb-2'
+          placeholder='Escribe tu tarea'
+          onChange={(e) => {
+            setTitle(e.target.value)
+          }}
+          value={title}
+          autoFocus
+        />
+        <textarea
+          className='bg-slate-300 p-3 w-full mb-2'
+          placeholder='Escribe una descripción'
+          onChange={(e) => {
+            setDescription(e.target.value)
+          }}
+          value={description}
+        ></textarea>
+        <button className='bg-indigo-500 px-3 py-1 text-white'>Guardar</button>
+      </form>
+    </div>
   )
 }
 
